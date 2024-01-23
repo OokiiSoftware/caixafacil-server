bdac631047cbd8e12eb6b6918d7b0d7382c779fa
OokiiSoftware <ookiisoftware@gmail.com>
2024-01-23 10:55:09 -0300

    Install json wen token

--- src/routes/empresa_routes.ts
+++ src/routes/empresa_routes.ts
@@ -1,31 +1,36 @@
 import { Router } from "express";
+import { verifyJWT } from "../util/JwToken";
 import Controller from "../controller/empresa_controller";
 
 class EmpresaRoute {
     allRoutes(Routes: Router) {
       Routes.get(
         "/empresa",
-        // verifyJWT,
+        verifyJWT,
         Controller.getListEmpresa
       );
       
       Routes.get(
         '/empresa/:Id',
+        verifyJWT,
         Controller.getEmpresaById
       );
   
       Routes.post(
         '/empresa',
+        verifyJWT,
         Controller.insertEmpresa
       );
 
       Routes.put(
         '/empresa/:Id',
+        verifyJWT,
         Controller.updateEmpresa
       );
   
       Routes.delete(
         '/empresa/:Id',
+        verifyJWT,
         Controller.deleteEmpresa
       );
     }
