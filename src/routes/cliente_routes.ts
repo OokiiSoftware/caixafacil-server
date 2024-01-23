ef1b84f3517b53ee88af9e85a9074fa218634be1
OokiiSoftware <ookiisoftware@gmail.com>
2024-01-23 10:27:35 -0300

    Install json web token

--- src/routes/cliente_routes.ts
+++ src/routes/cliente_routes.ts
@@ -1,31 +1,36 @@
 import { Router } from "express";
+import { verifyJWT } from "../util/JwToken";
 import Controller from "../controller/cliente_controller";
 
 class ClienteRoute {
     allRoutes(Routes: Router) {
       Routes.get(
         "/cliente",
-        // verifyJWT,
+        verifyJWT,
         Controller.getListCliente
       );
 
       Routes.get(
         '/cliente/:Id',
+        verifyJWT,
         Controller.getClienteById
       );
   
       Routes.post(
         '/cliente',
+        verifyJWT,
         Controller.insertCliente
       );
 
       Routes.put(
         '/cliente/:Id',
+        verifyJWT,
         Controller.updateCliente
       );
   
       Routes.delete(
         '/cliente/:Id',
+        verifyJWT,
         Controller.deleteCliente
       );
     }
